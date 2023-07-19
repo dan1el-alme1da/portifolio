6fb241f29a056fcfefd88f8a4cf9e9f508f8dc49
daniel almeida <daniel.segcorp@gmail.com>
2023-07-19 14:54:15 -0300

    menu responsivo

--- src/script/script.js
+++ src/script/script.js
@@ -1,4 +1,12 @@
-/*============================== scroll sections avtive link =============================*/
+/*============================= toggle nav bar =============================*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+};
+/*============================== scroll sections active link =============================*/
 let sections = document.querySelectorAll("section");
 let navlinks = document.querySelectorAll("header nav a");
 
